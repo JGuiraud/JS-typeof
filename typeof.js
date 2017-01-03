0d756529d756192898ffffbf64ce4bf0766b6945
JGuiraud <jerome31guiraud@gmail.com>
2017-01-03 11:56:16 +0100

    ajout de else if

--- typeof.js
+++ typeof.js
@@ -4,7 +4,7 @@ var b = "3";
 function pareil(a,b) {
  if (typeof a === typeof b) {
    console.log("les deux objets ont un type identique");
- } else {
+ } else /* if (typeof a != typeof b) */  {
    console.log("les deux objets ont un type différent");
    }
 }
